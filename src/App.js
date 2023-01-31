3a6d9c3cd4f3fe45d0e5d803f82c67e9c7addcd2
fedesouto <federicogsouto@gmail.com>
2023-01-31 12:57:21 -0300

    scrollToTop

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import Main from './components/Main/index';
-import Navbar from './components/Navbar/Navbar';
-import './styles.scss'
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import ContactForm from './components/Contact/Contact';
-import Services from './components/Services/Services';
-import Footer from './components/Footer/Footer';
+import Main from "./components/Main/index";
+import Navbar from "./components/Navbar/Navbar";
+import "./styles.scss";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import ContactForm from "./components/Contact/Contact";
+import Services from "./components/Services/Services";
+import Footer from "./components/Footer/Footer";
+import ScrollToTop from "./components/Helpers/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route index element={<Main />} />
-        <Route path='/contact' element={<ContactForm />} />
-        <Route path='/services' element={<Services />} />
-      </Routes>
-      <Footer />
+      <ScrollToTop>
+        <Navbar />
+        <Routes>
+          <Route index element={<Main />} />
+          <Route path="/contact" element={<ContactForm />} />
+          <Route path="/services" element={<Services />} />
+        </Routes>
+        <Footer />
+      </ScrollToTop>
     </BrowserRouter>
   );
 }
--- src/components/Main/Features.jsx
+++ src/components/Main/Features.jsx
@@ -1,30 +1,63 @@
 import React from "react";
-import {FcMultipleDevices, FcRating, FcStatistics, FcSupport} from 'react-icons/fc'
+import {
+  FcMultipleDevices,
+  FcRating,
+  FcStatistics,
+  FcSupport,
+} from "react-icons/fc";
 
 const Features = () => {
-    const elements = [
-        {icon: <FcMultipleDevices />, title: 'Lorem, ipsum dolor.', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde tenetur voluptatem quaerat iste cupiditate in.'},
-        {icon: <FcRating />, title: 'Lorem, ipsum dolor.', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde tenetur voluptatem quaerat iste cupiditate in.'},
-        {icon: <FcStatistics />, title: 'Lorem, ipsum dolor.', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde tenetur voluptatem quaerat iste cupiditate in.'},
-        {icon: <FcSupport />, title: 'Lorem, ipsum dolor.', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde tenetur voluptatem quaerat iste cupiditate in.'},
-    ]
-    return (
-        <div id="features">
-            <h2 className="features__title">Construimos sitios web que impulsan resultados.</h2>
-            <p className="features__copy">Utilizamos herramientas y tecnologías de vanguardia para crear sitios web seguros, escalables y de gran rendimiento. <br />Nos enfocamos en desarrollar sitios web que atraigan y conviertan a los clientes, a la vez que mejoran la usabilidad y la experiencia del usuario.</p>
-            <div className="features__wrapper container">
-            {elements.map(({icon, title, description}, index) => {
-                return(
-                    <div key={index} className="feature">
-                        {icon}
-                        <h3>{title}</h3>
-                        <p>{description}</p>
-                    </div>
-                )
-            })}
-        </div>
-        </div>
-    )
-}
+  const elements = [
+    {
+      icon: <FcMultipleDevices />,
+      title: "Lorem, ipsum dolor.",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde tenetur voluptatem quaerat iste cupiditate in.",
+    },
+    {
+      icon: <FcRating />,
+      title: "Lorem, ipsum dolor.",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde tenetur voluptatem quaerat iste cupiditate in.",
+    },
+    {
+      icon: <FcStatistics />,
+      title: "Lorem, ipsum dolor.",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde tenetur voluptatem quaerat iste cupiditate in.",
+    },
+    {
+      icon: <FcSupport />,
+      title: "Lorem, ipsum dolor.",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde tenetur voluptatem quaerat iste cupiditate in.",
+    },
+  ];
+  return (
+    <div id="features">
+      <h2 className="features__title">
+        Construimos sitios web que impulsan resultados.
+      </h2>
+      <p className="features__copy">
+        Utilizamos herramientas y tecnologías de vanguardia para crear sitios
+        web seguros, escalables y de gran rendimiento. <br />
+        Nos enfocamos en desarrollar sitios web que atraigan y conviertan a los
+        clientes, a la vez que mejoran la usabilidad y la experiencia del
+        usuario.
+      </p>
+      <div className="features__wrapper container">
+        {elements.map(({ icon, title, description }, index) => {
+          return (
+            <div key={index} className="feature">
+              {icon}
+              <h3>{title}</h3>
+              <p>{description}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Features;
