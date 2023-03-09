76bc3adfc7d4a0259a25b1b6d31598067ab61bc2
fedesouto <federicogsouto@gmail.com>
2023-03-09 15:51:08 -0300

    nuevo logo y colores

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -8,15 +8,6 @@ const Main = () => {
     <div>
       <div className="main container">
         <Hero />
-        <div
-          className="container"
-          style={{
-            width: "31vw",
-            height: "100%",
-          }}
-        >
-            <img src="https://cdni.iconscout.com/illustration/premium/thumb/web-development-3727526-3135828.png" alt="" style={{maxWidth: '100vw', height: 'auto'}}/>
-        </div>
       </div>
       <Features />
       <Clients />
